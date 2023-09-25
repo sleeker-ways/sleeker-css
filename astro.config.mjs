19b3643fae7862321a111094d5fe8bed685781b0
sleeker-auther <sleeker@eva.mozmail.com>
2023-09-24 20:13:30 -0700

    fix: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sleeker-ways.github.io',
+	site: 'https://sleeker-ways.github.io/sleeker-css-docs',
 	base: '/sleeker-css-docs',
 	integrations: [
 		starlight({
